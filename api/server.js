097007e624e6ca8a311f300913c83163090afd7b
NoaD159 <110129328+NoaD159@users.noreply.github.com>
2023-01-19 18:45:00 +0200

    error 500 on vercel cors option

--- api/server.js
+++ api/server.js
@@ -23,8 +23,12 @@ const uri = process.env.MONGODB_URI;
 //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 //   next();
 // });
-
-// app.use(cors());
+const corsOptions = {
+  origin: process.env.REACT_APP_BASE_URL,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors());
 app.use(express.json());
 
 mongoose.set("strictQuery", true);
