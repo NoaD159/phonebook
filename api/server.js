908d828633aa6a7069364fbd0728bd508d23af74
NoaD159 <110129328+NoaD159@users.noreply.github.com>
2023-01-19 18:51:35 +0200

    error 500 on vercel cors option2

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose.set("strictQuery", true);
