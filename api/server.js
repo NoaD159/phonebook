949f3d9d6ebd77ad5a91f2af4276df9ce11e5a34
NoaD159 <110129328+NoaD159@users.noreply.github.com>
2023-01-19 17:00:14 +0200

    error 500 on vercel try5

--- api/server.js
+++ api/server.js
@@ -13,13 +13,16 @@ require("dotenv").config({ path: "./.env" });
 const app = express();
 const port = process.env.REACT_APP_PORT || 8080;
 const uri = process.env.MONGODB_URI;
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Content-Security-Policy",
-//     "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://snap.licdn.com"
-//   );
-//   next();
-// });
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
