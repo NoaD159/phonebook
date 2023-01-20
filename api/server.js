d0bdd4ca3e9943af25ca5052b49cd248aaff10c0
NoaD159 <110129328+NoaD159@users.noreply.github.com>
2023-01-20 14:41:27 +0200

    refresh error try5

--- api/server.js
+++ api/server.js
@@ -14,13 +14,6 @@ const app = express();
 const port = process.env.REACT_APP_PORT || 8080;
 const uri = process.env.MONGODB_URI;
 
-// const corsOptions = {
-//   origin: process.env.REACT_APP_BASE_URL,
-//   credentials: true, //access-control-allow-credentials:true
-//   allowedHeaders: ["Content-Type", "Authorization"],
-
-//   optionSuccessStatus: 200,
-// };
 app.use(cors());
 app.use(express.json());
 
